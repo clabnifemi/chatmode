4ae050700d9e4827ab1669277b92857df567c08c
clabnifemi <nifemi@codedlab.com.ng>
2025-02-06 17:19:00 +0100

    updated: app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="text-red-500 bg-blue-700 font-bold h-full w-full">
-      <Button variant={"destructive"}>click me</Button>
+      <Button className="bg-slate-600">click me</Button>
       This is the user interface.
     </div>
   )
