3453b4653638a8943a2f6e7b50aad6baa46d861f
clabnifemi <nifemi@codedlab.com.ng>
2025-02-07 14:41:40 +0100

    Update for Clerk Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="text-red-500 bg-blue-700 font-bold h-full w-full">
-      <Button className="bg-slate-600">click me</Button>
+    <div>
       This is the user interface.
     </div>
   )
 }
+ 
